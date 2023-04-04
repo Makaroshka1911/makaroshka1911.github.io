d031f6d8ef97a03c916ab338a8ed5ee8d039eae4
Makaroshka1911 <112781283+Makaroshka1911@users.noreply.github.com>
2023-04-04 19:23:51 +0300

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,17 @@
+
+
+
+(function (window)
+{
+  var helloSpeaker =
+      {
+     speak: function  (name)
+        {
+      console.log(speakWord + " " + name);
+         }
+      }
+
+var speakWord = "Hello";
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
